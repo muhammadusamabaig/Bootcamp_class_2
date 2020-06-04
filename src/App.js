b1a0e50d45502dae92c27f26df21930355604cf0
muhammad usama baig <47364773+muhammadusamabaig@users.noreply.github.com>
2020-06-04 00:43:00 -0700

    after configure

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>src/App.js</code> and save to reload.
+           <code>src/App.js</code> osaaaaaaaaaaaaaaaaaaaaama reload.
         </p><p>osama</p>
         <a
           className="App-link"
